a5627966ffca0997595c0c9e0c9e797d180862af
tkuchiki <tkuchiki@lycorp.co.jp>
2026-02-19 18:10:56 +0900

    テストコードの課題完了

---
+++ 06_test/test_authenticator.py
@@ -0,0 +1,20 @@
+import pytest
+from authenticator import Authenticator
+
+@pytest.fixture
+def authenticator():
+    authenticator = Authenticator()
+    return authenticator
+
+def test_register(authenticator):
+    authenticator.register("user1", "pass1")
+    assert authenticator.users["user1"] == "pass1"
+
+    with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
+        authenticator.register("user1", "pass1")
+
+def test_login(authenticator):
+    authenticator.register("user1", "pass1")
+    assert authenticator.login("user1", "pass1") == "ログイン成功"
+    with pytest.raises(ValueError, match="エラー: ユーザー名またはパスワードが正しくありません。"):
+        authenticator.login("user1", "pass2")
